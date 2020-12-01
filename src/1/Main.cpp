7925486c6547eeeff836bd68ada973b6a685d83f
wessel <mastwessel@gmail.com>
2020-12-02 00:24:20 +0100

    added linked list sort to improve performance

--- src/1/Main.cpp
+++ src/1/Main.cpp
@@ -24,7 +24,74 @@ struct List {
     node->next = head;
     head = node;
   }
-  
+
+  void print() {
+    Node<T>* current = head;
+    while(current) {
+      printf("\t%d\n", current->data);
+      current = current->next;
+    }
+  }
+
+  Node<T>* get_tail(Node<T>* targetHead) {
+    while(targetHead && targetHead->next) {
+      targetHead = targetHead->next;
+    }
+    return targetHead;
+  }
+
+  Node<T>* quick_sort_internal(Node<T>* begin, Node<T>* tail) {
+    if(!begin || begin == tail) {
+      return begin;
+    }
+
+    Node<T>* current = begin; 
+    Node<T>* pivot = tail;
+    Node<T>* previous = nullptr;
+    Node<T>* newHead = nullptr;
+    Node<T>* newTail = nullptr;
+
+    while(current != pivot) {
+      if(current->data < pivot->data) {
+	if(!newHead) newHead = current;
+	previous = current;
+	current = current->next;
+      }
+      else {
+	if(previous) previous->next = current->next;
+	Node<T>* tmp = current->next;
+	current->next = nullptr;
+	tail->next = current;
+	tail = current;
+	current = tmp;
+      }
+    }
+
+    if(!newHead) newHead = pivot;
+    newTail = tail;
+
+    if(newHead != pivot) {
+      Node<T>* tmp = newHead;
+      while(tmp->next != pivot) {
+	tmp = tmp->next;
+      }
+      tmp->next = nullptr;
+
+      newHead = quick_sort_internal(newHead, tmp);
+
+      tmp = get_tail(newHead);
+      tmp->next = pivot;
+    }
+
+    pivot->next = quick_sort_internal(pivot->next, newTail);
+
+    return newHead;
+  }
+
+  void quick_sort() {
+    head = quick_sort_internal(head, get_tail(head));
+  }
+
   void free_list() {
     Node<T>* current = head;
     Node<T>* next = nullptr;
@@ -41,11 +108,26 @@ struct List {
   }
 };
 
+inline void sort_list(List<int32>& list) {
+  Node<int32>* current = list.head;
+  Node<int32>* next    = list.head;
+  while(current) {
+    next = current->next;
+    if(next->data < current->data) {
+      int32 temp = next->data;
+      next->data = current->data;
+      current->data = temp;
+    }
+    current = next;
+  }
+}
+
 inline int32 fetch_expense_report(const List<int32>& list) {
   Node<int32>* x = list.head;
   while(x) {
     Node<int32>* y = list.head;
     while(y) {
+      if(x->data + y->data > 2020) break;
       if(x->data + y->data == 2020) {
 	return x->data * y->data;
       }
@@ -64,6 +146,7 @@ inline int32 fetch_expense_report_deeper(const List<int32>& list) {
     while(y) {
       Node<int32>* z = list.head;
       while(z) {
+	if(x->data + y->data + z->data > 2020) break;
 	if(x->data + y->data + z->data == 2020) {
 	  return x->data * y->data * z->data;
 	}
@@ -88,6 +171,8 @@ int main() {
   List<int32> list;
   int32 line;
   while(file >> line) list.insert(line); 
+
+  list.quick_sort();
 
   printf("expense report 1: %d\n", fetch_expense_report(list));
   printf("expense report 2: %d\n", fetch_expense_report_deeper(list));
